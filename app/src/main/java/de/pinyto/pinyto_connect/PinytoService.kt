e90c2eb41f02c31a6efa9425abba07a2555ef683
Pina Merkert <pina@pinae.net>
2019-05-21 19:05:48 +0200

    It uses a message queue so no leaks occur

--- app/src/main/java/de/pinyto/pinyto_connect/PinytoService.kt
+++ app/src/main/java/de/pinyto/pinyto_connect/PinytoService.kt
@@ -1,11 +1,13 @@
 package de.pinyto.pinyto_connect
 
+import android.annotation.SuppressLint
 import android.app.Service
 import android.content.Intent
 import android.os.*
 import android.widget.Toast
 
 class PinytoService: Service() {
+    @SuppressLint("HandlerLeak")
     inner class IncomingHandler: Handler() {
         override fun handleMessage(msg: Message?) {
             super.handleMessage(msg)
